1759d76a27ff82a32cabfa9acbbb413c4da227e3
gjmrd <g.magomedov@skillaz.ru>
2020-10-24 19:46:56 +0300

    added 2 methods to base repository

--- CommunityBot/Persistence/RepositoryBase.cs
+++ CommunityBot/Persistence/RepositoryBase.cs
@@ -1,7 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.SQLite;
-using System.IO;
-using System.Reflection;
 using System.Threading.Tasks;
 using CommunityBot.Contracts;
 using Dapper;
@@ -10,17 +9,44 @@ namespace CommunityBot.Persistence
 {
     internal class RepositoryBase<TEntity> where TEntity : EntityBase, new()
     {
-        protected readonly SQLiteConnection Connection;
+        private readonly SQLiteConnection _connection;
         protected readonly string TableName = (Attribute.GetCustomAttribute(typeof(TEntity), typeof(TableAttribute)) as TableAttribute)!.TableName;
 
         public RepositoryBase(SQLiteConnection connection)
         {
-            Connection = connection;
+            _connection = connection;
         }
         
-        public async Task<TEntity> ById(string id)
+        protected async Task<TEntity> ById(string id)
         {
-            return await Connection.QuerySingleAsync<TEntity>("select * from {tableName} where id = {id}", new { tableName = TableName, id = id });
+            return await _connection.QuerySingleOrDefaultAsync<TEntity>("select * from {tableName} where id = {id}", new { tableName = TableName, id });
+        }
+
+        protected async Task<TEntity> GetSingle(string query, object parameters)
+        {
+            return await _connection.QuerySingleOrDefaultAsync<TEntity>(query, CreateQueryParams(parameters));
+        }
+
+        protected async Task<IEnumerable<TEntity>> GetList(string query, object parameters)
+        {
+            return await _connection.QueryAsync<TEntity>(query, CreateQueryParams(parameters));
+        }
+
+        protected async Task<IEnumerable<TEntity>> Insert(TEntity entity)
+        {
+            return await _connection.QueryAsync<TEntity>("insert into {tableName} name values {values}",
+                CreateQueryParams(entity));
+        }
+        
+        /// <summary>
+        /// Enriches parameters with params lite table name of db name
+        /// </summary>
+        /// <param name="parameters">object with query parameters</param>
+        /// <returns>enriched parameters object</returns>
+        private object CreateQueryParams(dynamic parameters)
+        {
+            parameters.tableName = TableName;
+            return parameters;
         }
     }
 }
