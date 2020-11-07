57aa38988c6e5d1479c79f5a6464cbcef0b954ed
MrModest <babaevkamil@gmail.com>
2020-11-07 14:05:04 +0300

    ensure DB when Start app

--- CommunityBot/Helpers/StartupExtensions.cs
+++ CommunityBot/Helpers/StartupExtensions.cs
@@ -1,8 +1,10 @@
 using System.Data.SQLite;
+using System.Linq;
 using CommunityBot.Contracts;
 using CommunityBot.Handlers;
 using CommunityBot.Persistence;
 using CommunityBot.Services;
+using Dapper;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Telegram.Bot;
@@ -45,8 +47,27 @@ namespace CommunityBot.Helpers
                 
                 var connection = new SQLiteConnection($"DataSource=\"{options.DbFilePath}\";");
                 connection.Open();
+                
+                EnsureDatabase(connection);
+                
                 return connection;
             });
+        }
+
+        private static void EnsureDatabase(SQLiteConnection connection)
+        {
+            var appTableNames = new[] {"SavedChats"};
+            
+            var tableNames = connection.Query<string>("SELECT name FROM sqlite_master WHERE type='table';");
+
+            foreach (var tableName in appTableNames.Except(tableNames))
+            {
+                if (tableName == "SavedChats")
+                {
+                    connection.Execute("CREATE TABLE IF NOT EXISTS main.SavedChats (Id INT PRIMARY KEY, ExactName TEXT NOT NULL, JoinLink TEXT DEFAULT NULL);");
+                    connection.Execute("CREATE INDEX IF NOT EXISTS main.ExactName_desc ON SavedChats (ExactName DESC);");
+                }
+            }
         }
     }
 }
