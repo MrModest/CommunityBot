f046da894a89ce36aa05c4bef0f66d6e64bf39a2
gjmrd <g.magomedov@skillaz.ru>
2020-11-08 23:47:27 +0300

    removed redunant code

--- CommunityBot/Program.cs
+++ CommunityBot/Program.cs
@@ -1,14 +1,12 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Serilog.Extensions.Logging;
 using static CommunityBot.Helpers.StartupExtensions;
 
 namespace CommunityBot
 {
     public class Program
     {
-        private static readonly LoggerProviderCollection Providers = new LoggerProviderCollection();
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
