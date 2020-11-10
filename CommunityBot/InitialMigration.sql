d1cae4cbce02eada075cfa714fad021961cee9fb
MrModest <babaevkamil@gmail.com>
2020-11-10 13:10:41 +0300

    delete unused sql script

--- CommunityBot/InitialMigration.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE IF NOT EXISTS main.SavedChats (
-                                               Id INT PRIMARY KEY,
-                                               ExactName TEXT NOT NULL,
-                                               JoinLink TEXT DEFAULT NULL
-);
-
-CREATE INDEX IF NOT EXISTS main.ExactName_desc ON SavedChats (ExactName DESC);
